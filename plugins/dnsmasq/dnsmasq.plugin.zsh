7112b05b91b6ea8fa8cca071c308816c8bf953cc
Bachue Zhou <bachue.shu@gmail.com>
2014-07-03 09:36:51 +0800

    add condition

--- plugins/dnsmasq/dnsmasq.plugin.zsh
+++ plugins/dnsmasq/dnsmasq.plugin.zsh
@@ -1,10 +1,12 @@
-dnsmasq_start () {
-    sudo launchctl start homebrew.mxcl.dnsmasq
-}
-dnsmasq_stop () {
-    sudo launchctl stop homebrew.mxcl.dnsmasq
-}
-dnsmasq_restart () {
-    dnsmasq_stop
-    dnsmasq_start
-}
+if [[ -x "`which dnsmasq`" ]]; then
+    dnsmasq_start () {
+        sudo launchctl start homebrew.mxcl.dnsmasq
+    }
+    dnsmasq_stop () {
+        sudo launchctl stop homebrew.mxcl.dnsmasq
+    }
+    dnsmasq_restart () {
+        dnsmasq_stop
+        dnsmasq_start
+    }
+fi
