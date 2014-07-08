f32ce8647aafda33727fe93b86cb9b55e1f2bf18
Bachue Zhou <bachue.shu@gmail.com>
2014-07-08 21:27:18 +0800

    simplify boot2docker port

--- plugins/mydocker/mydocker.plugin.zsh
+++ plugins/mydocker/mydocker.plugin.zsh
@@ -1,7 +1,5 @@
 if [[ -x "`which boot2docker`" ]]; then
-    if [[ "`boot2docker status`" = "running"  ]]; then
-        export DOCKER_HOST=tcp://$(boot2docker ip 2>/dev/null):2375
-    fi
+    export DOCKER_HOST=tcp://:2375
 
     alias lsdocker='docker images'
     alias psdocker='docker ps -a -q'
