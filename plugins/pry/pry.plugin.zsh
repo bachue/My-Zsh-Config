22c6b279abe61151a4660e0af19acacc3aed9a75
Bachue Zhou <bachue.shu@gmail.com>
2014-07-10 13:54:44 +0800

    customized plugin pry now supports rbenv

--- plugins/pry/pry.plugin.zsh
+++ plugins/pry/pry.plugin.zsh
@@ -7,6 +7,10 @@ pry () {
     `rvm gemdir`/bin/pry $@
   }
 
+  pry_with_rbenv () {
+    `rbenv which pry` $@
+  }
+
   pry_with_system () {
     /usr/bin/pry $@
   }
@@ -14,15 +18,19 @@ pry () {
   if [ -e ./config/environment.rb ]; then
     if [ -e Gemfile.lock ] && grep -q pry Gemfile.lock; then
       pry_with_bundler -r ./config/environment $@ 
-    elif [ "`command -v rvm`" != "" ] && [ -e "`rvm gemdir`/bin/pry" ]; then
+    elif [ "`command -v rvm`" != "" ] && [ -x "`rvm gemdir`/bin/pry" ]; then
       pry_with_rvm -r ./config/environment $@
+    elif [ "`command -v rbenv`" != "" ] && [ -x "`rbenv which pry`" ]; then
+      pry_with_rbenv -r ./config/environment $@
     elif [ -e "/usr/bin/pry" ]; then
       pry_with_system -r ./config/environment $@
     else
       echo 'pry not found' > /dev/stderr
     fi
-  elif [ "`command -v rvm`" != "" ] && [ -e "`rvm gemdir`/bin/pry" ]; then
+  elif [ "`command -v rvm`" != "" ] && [ -x "`rvm gemdir`/bin/pry" ]; then
     pry_with_rvm $@
+  elif [ "`command -v rbenv`" != "" ] && [ -x "`rbenv which pry`" ]; then
+    pry_with_rbenv $@
   elif [ -e "/usr/bin/pry" ]; then
     pry_with_system $@
   else
