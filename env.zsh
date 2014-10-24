b53f46aac36d95e1484401e8f84e94c6e87127d5
Bachue Zhou <bachue.shu@gmail.com>
2014-10-25 01:05:12 +0800

    add Linux manpage support on Dash

--- env.zsh
+++ env.zsh
@@ -1,2 +1,7 @@
 export EDITOR=vi
 export VIM_APP_DIR=/Applications
+
+LINUX_MANPATH="$HOME/Library/Application Support/Dash/Man Pages/Linux/man"
+if [ -d "$LINUX_MANPATH" ]; then
+    export MANPATH="$MANPATH:$LINUX_MANPATH"
+fi
