c9398e3dae16568ec8c4f0218848ec67d06ac865
Bachue Zhou <bachue.shu@gmail.com>
2014-10-27 11:25:24 +0800

    Add custom plugin rbenv-installer

---
+++ plugins/rbenv-installer/rbenv-installer.plugin.zsh
@@ -0,0 +1,18 @@
+if [ ! -d "$HOME/.rbenv" ]; then
+    install_rbenv() {
+        if [ -d "$HOME/.rbenv" ]; then
+            echo 'rbenv installed.' >&2
+        else
+            git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
+            git clone https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+            git clone https://github.com/sstephenson/rbenv-default-gems.git $HOME/.rbenv/plugins/rbenv-default-gems
+            git clone https://github.com/sstephenson/rbenv-gem-rehash.git $HOME/.rbenv/plugins/rbenv-gem-rehash
+            git clone https://github.com/rkh/rbenv-update.git $HOME/.rbenv/plugins/rbenv-update
+            git clone https://github.com/tpope/rbenv-aliases.git $HOME/.rbenv/plugins/rbenv-aliases
+            git clone https://github.com/bachue/rbenv-gemdir.git $HOME/.rbenv/plugins/rbenv-gemdir
+            echo "bundler" > $HOME/.rbenv/default-gems
+            rbenv alias --auto
+        fi
+    }
+fi
+
