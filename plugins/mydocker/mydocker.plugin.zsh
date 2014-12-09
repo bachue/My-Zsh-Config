5df3287a48a08a51d06f9f9a66ee64d9064036cd
Bachue Zhou <bachue.shu@gmail.com>
2014-12-09 11:28:52 +0800

    update docker variable

--- plugins/mydocker/mydocker.plugin.zsh
+++ plugins/mydocker/mydocker.plugin.zsh
@@ -1,5 +1,7 @@
 if [[ -x "`which boot2docker`" ]]; then
-    export DOCKER_HOST=tcp://:2375
+    export DOCKER_HOST=tcp://`boot2docker ip 2>/dev/null`:2376
+    export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
+    export DOCKER_TLS_VERIFY=1
 
     alias lsdocker='docker images'
     alias psdocker='docker ps -a -q'
