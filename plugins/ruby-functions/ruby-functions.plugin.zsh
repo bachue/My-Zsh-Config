fbe2eea1f215e2cac5bd9d269e4c6966e1376556
Bachue Zhou <bachue.shu@gmail.com>
2015-03-27 22:54:34 +0800

    beauty ruby-functions

--- plugins/ruby-functions/ruby-functions.plugin.zsh
+++ plugins/ruby-functions/ruby-functions.plugin.zsh
@@ -1,6 +1,6 @@
 if [[ -n "`which ruby`" ]]; then
-alias start-httpserver='ruby -run -e httpd .'
-function disasm() {
-    ruby -e "puts RubyVM::InstructionSequence.compile(File.read('$1')).disasm"
-}
+    alias start-httpserver='ruby -run -e httpd .'
+    function disasm() {
+        ruby -e "puts RubyVM::InstructionSequence.compile(File.read('$1')).disasm"
+    }
 fi
