f9cf26a022edb7f6ff3020ef4aa96fb98b4c0dde
Bachue Zhou <bachue.shu@gmail.com>
2015-03-27 21:56:43 +0800

    now `portdocker map/del` won't shutdown vm

--- plugins/mydocker/mydocker.plugin.zsh
+++ plugins/mydocker/mydocker.plugin.zsh
@@ -11,14 +11,14 @@ if [[ -x "`which boot2docker`" ]]; then
         MACHINENAME="boot2docker-vm"
         __portdocker_map() {
             echo "mapping $1 ..."
-            VBoxManage modifyvm $MACHINENAME --natpf1 "tcp-port$1,tcp,,$1,,$1";
-            VBoxManage modifyvm $MACHINENAME --natpf1 "udp-port$1,udp,,$1,,$1";
+            VBoxManage controlvm $MACHINENAME natpf1 "tcp-port$1,tcp,,$1,,$1"
+            VBoxManage controlvm $MACHINENAME natpf1 "udp-port$1,udp,,$1,,$1"
         }
 
         __portdocker_del() {
             echo "deleting $1 ..."
-            VBoxManage modifyvm $MACHINENAME --natpf1 delete "tcp-port$1";
-            VBoxManage modifyvm $MACHINENAME --natpf1 delete "udp-port$1";
+            VBoxManage controlvm $MACHINENAME natpf1 delete "tcp-port$1"
+            VBoxManage controlvm $MACHINENAME natpf1 delete "udp-port$1"
         }
 
         case $1 in
