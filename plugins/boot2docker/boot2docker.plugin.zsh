880ea8c8df76cacc0958b62168c370542faf6387
Bachue Zhou <bachue.shu@gmail.com>
2015-06-30 00:21:51 +0800

    autorun cmd to fix boot2docker 1.7

--- plugins/boot2docker/boot2docker.plugin.zsh
+++ plugins/boot2docker/boot2docker.plugin.zsh
@@ -1,4 +1,4 @@
 if [[ -x "`which boot2docker`" ]]; then
     $(boot2docker shellinit 2>/dev/null)
-    alias startdocker='boot2docker start && `boot2docker shellinit 2>/dev/null`'
+    alias startdocker='boot2docker start && `boot2docker shellinit 2>/dev/null` && boot2docker ssh "sudo /etc/init.d/docker restart"'
 fi
