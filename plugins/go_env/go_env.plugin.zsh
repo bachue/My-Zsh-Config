0981304389d67680aac2665103a2db379ca54157
Bachue Zhou <bachue.shu@gmail.com>
2016-02-20 11:47:19 +0800

    migrate go env to ~/GoEnv

--- plugins/go_env/go_env.plugin.zsh
+++ plugins/go_env/go_env.plugin.zsh
@@ -1,32 +1,11 @@
 if [[ -x "`which go`" ]]; then
-    create_go_project() {
-        TARGET=$1
-        if [[ -z "$TARGET" ]]; then
-            echo "Usage: create_go_project project_name" 1>&2
-            return
-        fi
-
-        echo "Create Go Project at $TARGET"
-
-        mkdir -p $TARGET &&
-        cd $TARGET &&
-        mkdir src bin pkg &&
-        export GOPATH=$GOPATH:`pwd` &&
-        echo "Environment GOPATH set" || echo "Create Project $TARGET Failed" 1>&2
-    }
-
-    go_project() {
-        cd $1 &&
-        export GOPATH=$GOPATH:`pwd`
-    }
-
     GOROOT=`go env GOROOT`
     if [ -n "$GOROOT" ]; then
         export PATH=$GOROOT/bin:$PATH
     fi
 
     if [ -z "$DEFAULT_GOPATH" ]; then
-        export DEFAULT_GOPATH=$HOME/Projects/go_projects
+        export DEFAULT_GOPATH=$HOME/GoEnv
     fi
 
     if [ -d "$DEFAULT_GOPATH" ]; then
